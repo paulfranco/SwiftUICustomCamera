b836b3af18b6169e2c18052379be1b86d41cf337
Paul Franco <mail@paulfran.co>
2021-03-13 01:49:22 -0800

    Initial Commit

---
+++ SwiftUiCustomCamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiCustomCamera
+//
+//  Created by Paul Franco on 3/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUiCustomCamera/SwiftUiCustomCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCustomCameraApp.swift
+//  SwiftUiCustomCamera
+//
+//  Created by Paul Franco on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCustomCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
